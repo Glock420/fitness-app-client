a162a9710747bcccd3075484f22057b6b4b52375
cliffabella108 <cliffabella108@gmail.com>
2024-12-16 14:53:15 +0800

    Edit workouts page nav login

--- src/pages/Workouts.js
+++ src/pages/Workouts.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Row, Col, Card, Button } from 'react-bootstrap';
 
 import UserContext from '../context/UserContext';
@@ -26,29 +26,33 @@ export default function ProductCatalog() {
 	}, [])
 
 	return(
-		<>
-			<h1 className="my-5">My Workouts</h1>
-			<Row className="d-flex justify-content-center mx-auto my-5">
-				{
-					(workouts.length > 0)
-					?
-						workouts.map(workout => {
-							return(
-								<Col className="col-3 justify-content-center mt-4">
-					                <Card>
-										<Card.Body>
-										    <Card.Title className="mb-4 text-center">{workout.name}</Card.Title>
-										    <Card.Text className="mb-3">Duration: {workout.duration}</Card.Text>
-										    <Card.Text className="mb-1">Status: {workout.status}</Card.Text>
-										</Card.Body>
-									</Card>
-					            </Col>
-							)
-						})
-					:
-						<h1>No Planned Workouts :(</h1>
-				}
-	        </Row>
-        </>
+		(user.id !== null)
+		?
+			<>
+				<h1 className="my-5">My Workouts</h1>
+				<Row className="d-flex justify-content-center mx-auto my-5">
+					{
+						(workouts.length > 0)
+						?
+							workouts.map(workout => {
+								return(
+									<Col className="col-3 justify-content-center mt-4">
+						                <Card>
+											<Card.Body>
+											    <Card.Title className="mb-4 text-center">{workout.name}</Card.Title>
+											    <Card.Text className="mb-3">Duration: {workout.duration}</Card.Text>
+											    <Card.Text className="mb-1">Status: {workout.status}</Card.Text>
+											</Card.Body>
+										</Card>
+						            </Col>
+								)
+							})
+						:
+							<h1>No Planned Workouts :(</h1>
+					}
+		        </Row>
+	        </>
+	    :
+	    	<Navigate to='/login' />
 	);
 }
